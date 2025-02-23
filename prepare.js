8e85ad6aa55655beaa88f3a6a5975a3c983de350
D N <4661784+retyui@users.noreply.github.com>
2025-02-23 16:10:03 +0100

    add duplicates map

--- prepare.js
+++ prepare.js
@@ -1,16 +1,60 @@
-function prepare(data, { markers, crejectData, setWorkTitle }) {
-  const moduleMap = new Map();
+function getDuplicateId(path) {
+  const uniqueNodeModulePath = path.split("node_modules/").pop();
+  const ids = [uniqueNodeModulePath];
+
+  // Special case for lodash
+  if (
+    uniqueNodeModulePath.startsWith("lodash.") ||
+    uniqueNodeModulePath.startsWith("lodash-es/")
+  ) {
+    // node_modules/lodash.debounce/index.js => node_modules/lodash/debounce.js
+    // node_modules/lodash-es/get.js         => node_modules/lodash/get.js
+    const uniqueLodashPath = uniqueNodeModulePath
+      .replace("lodash-es/", "lodash/")
+      .replace("lodash.", "lodash/")
+      .replace("/index.js", ".js");
+
+    ids.push(uniqueLodashPath);
+  }
+
+  return ids;
+}
+
+function prepare(data) {
+  let moduleMap = new Map();
+  let duplicatesMap = new Map();
+  let allLodashModules = new Set();
 
   data.modules.forEach((m) => {
+    // 0. Data transformation
     m.absolutePath = m.path;
     m.path = m.path.replace(data.rootFolder + "/", "");
-    m.dependents = [];
     m.dependencies.forEach((d) => {
       d.path = d.absolutePath.replace(data.rootFolder + "/", "");
     });
 
+    // 1. Duplicates
+    m._tmp_ids = getDuplicateId(m.path);
+    m.duplicates = [];
+    m._tmp_ids.forEach((id) => {
+      if (!duplicatesMap.has(id)) {
+        duplicatesMap.set(id, []);
+      }
+      duplicatesMap.get(id).push(m);
+    });
+    // lodash/*
+    // lodash-es/*
+    // lodash.*
+    if (m.path.includes("node_modules/lodash")) {
+      allLodashModules.add(m);
+    }
+
+    // 2. Dependencies
+    m.dependents = [];
     moduleMap.set(m.absolutePath, m);
 
+    // 3. Other
+    // 3.1 Format prelude
     if (m.path === "__prelude__") {
       m.source.code = m.source.code
         .replaceAll(";", ";\n\n")
@@ -22,6 +66,21 @@ function prepare(data, { markers, crejectData, setWorkTitle }) {
   });
 
   data.modules.forEach((m) => {
+    // 1. Duplicates
+    m._tmp_ids.forEach((id) => {
+      const duplicates = duplicatesMap.get(id);
+      if (duplicates.length > 1) {
+        m.duplicates.push(...duplicates.filter((d) => d !== m));
+      }
+    });
+
+    // lodash/index.js is a special case (as index.js includes all lodash functions)
+    if (m.path.endsWith("node_modules/lodash/index.js")) {
+      m.duplicates.push(...allLodashModules);
+    }
+    delete m._tmp_ids;
+
+    // 2. Dependencies
     m.dependencies.forEach((dependency) => {
       const dependentModule = moduleMap.get(dependency.absolutePath);
       if (dependentModule) {
@@ -30,6 +89,9 @@ function prepare(data, { markers, crejectData, setWorkTitle }) {
     });
   });
 
+  allLodashModules = null;
+  moduleMap = null;
+  duplicatesMap = null;
   return data;
 }
 
