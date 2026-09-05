de714352a7e8821fc8604cee32201132942a78d7
D N <4661784+retyui@users.noreply.github.com>
2026-09-05 22:31:11 +0200

    Babel runtime inline

--- _cli/recommendations/index.js
+++ _cli/recommendations/index.js
@@ -2,10 +2,12 @@ const reactNativeLinearGradient = require("./linear-gradient.js");
 const reactNativeRadialGradient = require("./radial-gradient.js");
 const duplicatePackages = require("./duplicate-packages.js");
 const abortControllerPolyfill = require("./rn-issue-abort-controller.js");
+const babelRuntimeInlineHelpers = require("./rn-issue-babel-runtime.js");
 
 module.exports = [
   reactNativeLinearGradient,
   reactNativeRadialGradient,
   duplicatePackages,
   abortControllerPolyfill,
+  babelRuntimeInlineHelpers,
 ];
