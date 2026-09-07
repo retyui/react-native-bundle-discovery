ddf5b03410358e8833b3770316bc891b81ebb776
D N <4661784+retyui@users.noreply.github.com>
2026-09-07 11:07:29 +0200

    No dev files

--- _cli/recommendations/index.js
+++ _cli/recommendations/index.js
@@ -8,6 +8,7 @@ const promisePolyfill = require("./rn-issue-promise-polyfill.js");
 const devOnlyPackages = require("./dev-only-packages.js");
 const rendererArchitectureMismatch = require("./no-skip-fabric-modules.js");
 const packageJsonFiles = require("./package-json-files.js");
+const devFiles = require("./dev-files.js");
 
 module.exports = [
   reactNativeLinearGradient,
@@ -20,4 +21,5 @@ module.exports = [
   devOnlyPackages,
   rendererArchitectureMismatch,
   packageJsonFiles,
+  devFiles,
 ];
